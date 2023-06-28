95590509e1d092152872bf822442e35ca202fe48
tomer shaw <shaw.tomer@gmail.com>
2023-06-28 16:07:42 +0300

    end of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context=context;
+        }
+
+         [HttpGet]
+           public async Task<ActionResult<List<Activity>>> GetActivites()
+           {
+            return await _context.Activities.ToListAsync();
+           }
+
+            [HttpGet("{id}")]
+            public async Task<ActionResult<Activity>> GetActivites(Guid id)
+            {
+                return await _context.Activities.FindAsync(id);
+
+            }
+    }
+
+ 
+}
